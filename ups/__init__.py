cca331726d38ae2f220e2f2671df5fb1f5372cd6
mcflugen <mcflugen@gmail.com>
2019-10-22 23:41:47 -0600

    import UpsCredentials with package

--- ups/__init__.py
+++ ups/__init__.py
@@ -1,7 +1,7 @@
 from ._version import get_versions
-from .xav import Address, UpsXav
+from .xav import Address, UpsCredentials, UpsXav
 
-__all__ = ["Address", "UpsXav"]
+__all__ = ["Address", "UpsCredentials", "UpsXav"]
 
 __version__ = get_versions()["version"]
 del get_versions
