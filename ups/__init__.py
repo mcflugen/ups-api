b223f734534d3fd8ede2a411557df03bbb9e1fc7
mcflugen <mcflugen@gmail.com>
2019-10-23 12:50:37 -0600

    import UpsXavBatch with package

--- ups/__init__.py
+++ ups/__init__.py
@@ -1,7 +1,7 @@
 from ._version import get_versions
-from .xav import Address, UpsCredentials, UpsXav
+from .xav import Address, UpsCredentials, UpsXav, UpsXavBatch
 
-__all__ = ["Address", "UpsCredentials", "UpsXav"]
+__all__ = ["Address", "UpsCredentials", "UpsXav", "UpsXavBatch"]
 
 __version__ = get_versions()["version"]
 del get_versions
